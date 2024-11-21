9d203d60fc751f8046392040126409a66201fc55
Rudolf Batt <rudolf.batt@commerce-experts.com>
2024-11-21 11:28:06 +0100

    Add remark regarding https links

--- de-to-swg.sh
+++ de-to-swg.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# Keep in mind, that lines with a https link are not touched at all. Either translate them manually or put the link on a separate line (in Markdown a single linebreak does not create a paragraph or linebreak in the final html doc)
 sed -r -f swabian.sed ui/readme.de.md > ui/readme.swg.md
